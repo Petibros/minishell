98c4c91e58a6092e96d68be68d91b1bd682a741e
NikaPapashvili <papashvili.n7@gmail.com>
2025-03-05 20:43:05 +0100

    Create cmds structure

--- src/minishell.h
+++ src/minishell.h
@@ -10,18 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <linux/limits.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdbool.h>
-#include <readline/history.h>
-#include <readline/readline.h>
-#include "libft.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <linux/limits.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <stdbool.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include "libft.h"
+
+typedef struct s_cmds
+{
+	char	**cmds;
+	char	**argvs;
+	char	**envp;
+	int		fd_in;
+	int		fd_out;
+}	t_cmds;
 
 typedef struct s_line
 {
