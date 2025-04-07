8db9e1923ca4d7f750b345fc19bbab9c1c2fbaad
NikaPapashvili <papashvili.n7@gmail.com>
2025-04-08 01:05:26 +0200

    Fix env var name getting

--- src/parsing/parser/parse_line.c
+++ src/parsing/parser/parse_line.c
@@ -14,6 +14,31 @@
 #include "parsing.h"
 #include "print_ast.h"
 
+static int	validate_syntax(t_token *tokens)
+{
+	t_token	*current;
+	t_token	*next;
+
+	if (!tokens)
+		return (0);
+	current = tokens;
+	if (current->type == TOKEN_PIPE)
+		return (0);
+	while (current)
+	{
+		next = current->next;
+		if (current->type == TOKEN_PIPE && (!next || next->type == TOKEN_PIPE))
+			return (0);
+		if ((current->type == TOKEN_REDIR_IN || current->type == TOKEN_REDIR_OUT
+				|| current->type == TOKEN_APPEND
+				|| current->type == TOKEN_HEREDOC) && (!next
+				|| next->type != TOKEN_WORD))
+			return (0);
+		current = next;
+	}
+	return (1);
+}
+
 int	parse_line(t_vars *vars)
 {
 	t_token	*tokens;
@@ -23,6 +48,11 @@ int	parse_line(t_vars *vars)
 	tokens = lexer(vars->line);
 	if (!tokens)
 		return (0);
+	if (!validate_syntax(tokens))
+	{
+		free_token(tokens);
+		return (0);
+	}
 	expand_variables_in_tokens(tokens, vars->cmd.last_exit_status, vars->env.envp);
 	vars->cmd.cmds = pratt_parse(tokens, vars->env.envp);
 	if (!vars->cmd.cmds)
