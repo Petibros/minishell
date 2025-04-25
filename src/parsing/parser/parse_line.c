cb56fea535bf64d68808de6703b657cab440a92b
NikaPapashvili <papashvili.n7@gmail.com>
2025-04-25 11:31:29 +0200

    Fix norm

--- src/parsing/parser/parse_line.c
+++ src/parsing/parser/parse_line.c
@@ -6,12 +6,40 @@
 /*   By: npapash <npapash@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 17:55:37 by npapash           #+#    #+#             */
-/*   Updated: 2025/04/25 11:20:03 by npapash          ###   ########.fr       */
+/*   Updated: 2025/04/25 11:27:42 by npapash          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parsing.h"
+
+static int	handle_syntax_error(t_token *tokens, t_vars *vars)
+{
+	free_token(tokens);
+	vars->cmd.last_exit_status = 2;
+	return (0);
+}
+
+static int	handle_parse_error(t_token *tokens, t_vars *vars)
+{
+	free_token(tokens);
+	if (vars->cmd.last_exit_status == 2)
+		return (0);
+	return (1);
+}
+
+static int	process_tokens(t_token *tokens, t_vars *vars)
+{
+	expand_variables_in_tokens(&tokens, vars->cmd.last_exit_status,
+		vars->env.envp);
+	vars->cmd.cmds = pratt_parse(tokens, vars->env.envp, vars);
+	if (!vars->cmd.cmds)
+		return (handle_parse_error(tokens, vars));
+	expand_wildcards(vars->cmd.cmds);
+	handle_quotes_in_node(vars->cmd.cmds);
+	free_token(tokens);
+	return (1);
+}
 
 int	parse_line(t_vars *vars)
 {
@@ -23,23 +51,6 @@ int	parse_line(t_vars *vars)
 	if (!tokens)
 		return (1);
 	if (!validate_syntax(tokens))
-	{
-		free_token(tokens);
-		vars->cmd.last_exit_status = 2;
-		return (0);
-	}
-	expand_variables_in_tokens(&tokens, vars->cmd.last_exit_status,
-		vars->env.envp);
-	vars->cmd.cmds = pratt_parse(tokens, vars->env.envp, vars);
-	if (!vars->cmd.cmds)
-	{
-		free_token(tokens);
-		if (vars->cmd.last_exit_status == 2)
-			return (0);
-		return (1);
-	}
-	expand_wildcards(vars->cmd.cmds);
-	handle_quotes_in_node(vars->cmd.cmds);
-	free_token(tokens);
-	return (1);
+		return (handle_syntax_error(tokens, vars));
+	return (process_tokens(tokens, vars));
 }
